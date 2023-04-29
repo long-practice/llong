6a5a91eb29212f03e99a0f7a821a2c1712dd7796
김종도 <llong@gimjongdoui-MacBookPro.local>
2023-04-29 21:02:43 +0900

    delete legacy files in Leetcode DIR

--- Algorithm/Algo_Problem/Leetcode/621_JD_2.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int:
-        task_count = Counter(tasks).values()
-        max_task = max(task_count)
-        max_count = sum(1 for c in task_count if c == max_task)
-        return max((n + 1) * (max_task - 1) + max_count, len(tasks))
